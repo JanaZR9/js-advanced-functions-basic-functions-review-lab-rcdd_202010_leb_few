438c814507e7f582db7ea449c00a0301678eb22a
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-05 10:11:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+//1
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+//2
+function mondayWork(act="go to the office"){
+  return `This Monday, I will ${act}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+//3 
+function wrapAdjective(car="*"){
+  return function(desc="special"){
+    return `You are ${car}${desc}${car}!`;
+  }
+}
+
+const Calculator{
+  
